c5c99858fd4c0c57d15762823e3a2a8435cceb0d
Karol Majek <karolmajek@gmail.com>
2017-02-22 08:31:21 +0100

    feat: export images + csv with speed=0

--- extract-from-video/extract.py
+++ extract-from-video/extract.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python3
-from collections import deque
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 import numpy as np
 import cv2
+import os
 import sys
+import csv
 import json
 
+#format: (xmin,xmax,ymin,ymax)
 throttle_window=(277,520,690,691)
 steering_left_window=(540,654,686,687)
 steering_right_window=(656,735,686,687)
 brakes_window=(770,970,686,687)
+save_window=(0,-1,280,600)
 
-def main():
-    fname = sys.argv[1]
-    cap = cv2.VideoCapture(fname)
-    # Define the codec and create VideoWriter object
-    # fourcc = cv2.VideoWriter_fourcc(*'X264')
-    # video_out = cv2.VideoWriter('results/'+fname.split('/')[-1],fourcc, 25, (1280,720))
+#csv arrays - zip to get rows
+throttle_csv=[]
+steering_csv=[]
+brakes_csv=[]
+speed_csv=[]
+filenames_csv=[]
 
-    if cap.isOpened():
-        ret, img = cap.read()
-        if not img is None:
-            cv2.imshow('img',img)
-            cv2.waitKey(1)
+#show data?
+show=False
 
+#output images dir
+img_dir='images'
+try:
+    os.mkdir(img_dir)
+except IOError as e:
+    if e.errno!=17:
+        print(e)
+        raise
+    else:
+        print('Directory "%s"'%img_dir,'already exists! Will overwrite files!')
+
+def dataGen(cap):
     while(cap.isOpened()):
         ret, img = cap.read()
         if not img is None:
@@ -55,16 +68,47 @@ def main():
             br[br>80]=255
             brakes=1.09*np.sum(br)/(255.0 * (brakes_window[1]-brakes_window[0]))
 
+            #TODO: implement speed reading
+            speed=0
+
+            #Save img
+            fname=img_dir+'/center_%08d.jpg'%len(filenames_csv)
+            to_save=img[save_window[2]:save_window[3],save_window[0]:save_window[1],:]
+            cv2.imwrite(fname,to_save)
+
+            throttle_csv.append(throttle_value)
+            steering_csv.append(steering)
+            brakes_csv.append(brakes)
+            speed_csv.append(speed)
+            filenames_csv.append(fname)
+
             font = cv2.FONT_HERSHEY_SIMPLEX
             cv2.putText(img,'%.0f'%(100*throttle_value),(throttle_window[0]+100,718), font, 1,(0,0,255),2,cv2.LINE_AA)
             cv2.putText(img,'%.0f'%(100*steering),(steering_right_window[0],718), font, 1,(0,0,255),2,cv2.LINE_AA)
             cv2.putText(img,'%.0f'%(100*brakes),(brakes_window[0],718), font, 1,(0,0,255),2,cv2.LINE_AA)
-            cv2.imshow('img',img)
-            cv2.waitKey(50)
-        # write result
-        # video_out.write(res)
+            if show:
+                cv2.imshow('img',img)
+                cv2.waitKey(1)
+
+            row=[fname,'','',steering,throttle_value,brakes,speed]
+            yield row
+
+
+def main():
+    fname = sys.argv[1]
+    cap = cv2.VideoCapture(fname)
+    length=int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    with open('output.csv', "w") as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        counter=0
+        total=length-20
+        for row in tqdm(dataGen(cap),total=total): #without last 20 frames...
+            writer.writerow(row)
+            counter=counter+1
+            if counter>total:
+                break
     cap.release()
-    # video_out.release()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
