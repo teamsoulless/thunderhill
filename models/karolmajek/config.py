a22a99643fb3a4e8f62d29724f7258fa01810d1e
Karol Majek <karolmajek@gmail.com>
2017-03-13 07:15:38 +0100

    based on nando's code

---
+++ models/karolmajek/config.py
@@ -0,0 +1,12 @@
+BATCH_SIZE = 128
+EPOCHS = 100
+FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+# WIDTH = 66
+# HEIGHT = 200
+WIDTH = 320
+HEIGHT = 160
+DEPTH = 3
+DEPTH = 3
+ALPHA = 0.001
+DROPOUT = 0.5
+OUTPUT = '.hdf5_checkpoints'
