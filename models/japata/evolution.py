adb5ad418ee8c35b40e7df63c8426daaed78acc0
Jacob <jpthalman@gmail.com>
2017-03-19 20:58:18 -0600

    Finish Individual and Population classes and define training function

--- models/japata/evolution.py
+++ models/japata/evolution.py
@@ -1,14 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import trange
 from scipy.spatial import distance
 import models
 
 
 class Individual(object):
-    def __init__(self, n_params, mu, sigma, mutation_prob):
-        self._size = n_params
+    def __init__(self, model_generator, mu, sigma, mutation_prob, fitness_func):
+        self.model = model_generator()
+        self._model_generator = model_generator
+        self._layer_shapes = [layer.shape for layer in self.model.get_weights()]
+        self._size = self.model.count_params()
         self._distribution = (mu, sigma)
         self._mutation_prob = mutation_prob
-        self.weights = np.random.normal(mu, sigma, n_params)
+        self._fitness_func = fitness_func
+        self.weights = np.random.normal(mu, sigma, self._size)
         self.fitness = None
 
     # mutate
@@ -43,103 +49,147 @@ class Individual(object):
             return self_mean < other_mean
         return self.fitness > other.fitness
 
-    def evaluate_fitness(self, paths):
-        # TODO
-        pass
+    def evaluate_fitness(self, generator):
+        self._update_model_weights()
+
+        feature_vectors = None
+        for batch in generator:
+            if feature_vectors is None:
+                feature_vectors = self.model.predict(batch)
+            else:
+                feature_vectors = np.concatenate((feature_vectors, self.model.predict(batch)))
+
+        self.fitness = self._fitness_func(feature_vectors)
 
     def set_mutation_prob(self, mutation_prob):
         self._mutation_prob = mutation_prob
 
-    def reset_weights(self):
+    def reset(self):
+        self.model = self._model_generator()
         self.weights = np.random.normal(*self._distribution, self._size)
+        self._update_model_weights()
+        self.fitness = None
+
+    def get_model(self):
+        self._update_model_weights()
+        return self.model
+
+    def _update_model_weights(self):
+        reshaped_weights = []
+        start_idx = 0
+
+        for idx in range(len(self._layer_shapes)):
+            shape = self._layer_shapes[idx]
+            end_idx = start_idx + np.prod(shape)
+            reshaped_weights.append(
+                self.weights[start_idx:end_idx].reshape(shape)
+            )
+            start_idx = end_idx
+
+        self.model.set_weights(reshaped_weights)
 
 
 class Population(object):
     def __init__(self, size, base_individual, mate_prob):
         self._size = size
-        self.mate_prob = mate_prob
-        self.population = [base_individual.reset_weights() for _ in range(size)]
+        self._mate_prob = mate_prob
+        self._population = [base_individual.reset() for _ in range(size)]
 
     def tournament(self, size):
         new_population = []
         for _ in range(self._size):
             new_population.append(max(
-                np.random.choice(self.population, size)
+                np.random.choice(self._population, size)
             ))
-        self.population = new_population
+        self._population = new_population
 
-    def evolve(self, method, paths):
-        assert method in ('and', 'or'), 'Method must be "and" or "or".'
-
+    def evolve(self, method, generator):
         if method == 'and':
-            self._random_mating(self.mate_prob)
+            self._random_mating()
             self._random_mutation()
-        else:
+        elif method == 'or':
             if np.random.uniform() < 0.5:
                 self._random_mutation()
             else:
-                self._random_mating(self.mate_prob)
+                self._random_mating()
+        else:
+            raise ValueError('Method must be "and" or "or".')
 
-        for individual in self.population:
+        for individual in self._population:
             if individual.fitness is None:
-                individual.evaluate_fitness(paths)
+                individual.evaluate_fitness(generator)
 
-    def _random_mating(self, prob):
-        for individual, mate in self._random_mate_generator(self.population):
-            if np.random.uniform() < prob:
+    @property
+    def leader(self):
+        return max(self._population)
+
+    def _generate_random_mates(self):
+        indexes = np.arange(len(self._population))
+        for ind in indexes:
+            mate = np.random.choice(np.delete(indexes, ind))
+            yield self._population[ind], self._population[mate]
+
+    def _random_mating(self):
+        for individual, mate in self._generate_random_mates():
+            if np.random.uniform() < self._mate_prob:
                 individual * mate
 
     def _random_mutation(self):
-        for individual in self.population:
+        for individual in self._population:
             ~individual
-
-    @staticmethod
-    def _random_mate_generator(array):
-        indexes = np.arange(len(array))
-        for ind in indexes:
-            mate = np.random.choice(np.delete(indexes, ind))
-            yield array[ind], array[mate]
-
-
-def get_layer_shapes(model):
-    return [layer.shape for layer in model.get_weights()]
-
-
-def construct_layers(weights, layer_shapes):
-    layer_shape_sum = sum([np.prod(layer) for layer in layer_shapes])
-    assert layer_shape_sum == weights.shape[0], 'Dimension mismatch.'
-
-    reshaped_weights = []
-    start_idx = 0
-    for idx in range(len(layer_shapes)):
-        shape = layer_shapes[idx]
-        end_idx = start_idx + np.prod(shape)
-        reshaped_weights.append(
-            weights[start_idx:end_idx].reshape(shape)
-          )
-    return reshaped_weights
 
 
 def fitness_function(array):
     pairwise_euclidean_distances = distance.pdist(array, 'euclidean')
-
-
-def evaluate_fitness(individual, model, generator, fitness_func):
-    # Get weights from individual and transform into layers
-    # Load weights into model
-    # Get output from the model
-    # Evaluate the fitness and store in the individual
+    # TODO
     pass
 
 
-if __name__ == '__main__':
-    model = models.cg23()
+def thunderhill_generator():
+    # TODO
+    pass
 
+
+def train(generator,
+          population,
+          tournament_size,
+          mate_and_or_mutate,
+          max_generations,
+          patience,
+          min_delta=1e-5):
+    fitness_history = []
+    prev_fitness = 0
+    generations_without_improvement = 0
+    leader = None
+
+    generations = trange(range(max_generations))
+    for gen in generations:
+        generations.set_description('GEN %i' % gen)
+
+        population.tournament(size=tournament_size)
+        population.evolve(method=mate_and_or_mutate, generator=generator)
+
+        leader = population.leader
+        fitness_history.append(leader.fitness)
+
+        fitness_delta = fitness_history[-1] - prev_fitness
+        if fitness_delta <= min_delta:
+            generations_without_improvement += 1
+            if generations_without_improvement == patience:
+                break
+
+        generations.set_postfix(fitness=fitness_history[-1], last_improvement=generations_without_improvement)
+        prev_fitness = fitness_history[-1]
+    return leader.get_model(), fitness_history
+
+
+if __name__ == '__main__':
     individual = Individual(
-        n_params=model.count_params(),
+        model_generator=models.cg23,
         mu=0,
         sigma=1.5,
-        mutation_prob=0.05
+        mutation_prob=0.05,
+        fitness_func=fitness_function
       )
     population = Population(
         size=100,
@@ -147,5 +197,16 @@ if __name__ == '__main__':
         mate_prob=0.8
       )
 
-    population.tournament(size=10)
-    population.evolve(method='and', paths=None)
+    best_model, history = train(
+        generator=thunderhill_generator,
+        population=population,
+        tournament_size=10,
+        mate_and_or_mutate='and',
+        max_generations=100,
+        patience=5,
+        min_delta=1e-5
+      )
+
+    plt.plot(history)
+    plt.title('Fitness History')
+    best_model.save('feature_extractor.h5')
