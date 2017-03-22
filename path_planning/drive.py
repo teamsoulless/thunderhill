042e81245b4f6f0b9c674c0aada8ad30ccd1ef0e
Maruf Maniruzzaman <kuasha@gmail.com>
2017-03-22 13:03:06 -0700

    Separatemodel for PID controller

--- path_planning/drive.py
+++ path_planning/drive.py
@@ -18,6 +18,7 @@ from robot import *
 from geo import *
 from drawing import *
 from dataprovider import *
+from model import *
 
 # tf.python.control_flow_ops = tf
 
@@ -39,108 +40,33 @@ for idx, p in enumerate (position_data[280:]):
 tpidx = 2
 DEG25 = deg2rad(25) 
 
-taup = 0.001
-taud = 0.01
-taui = 0.0
-int_cte = 0.0
 
 sio = socketio.Server()
 app = Flask(__name__)
-model = None
-prev_image_array = None
 
-def get_cte(keypoints, cp):
-    prev=keypoints[0]
-    tp = (keypoints[0], keypoints[1])
-    tpidx = 1
-    mind = 50000.
-    for idx, kp in enumerate(keypoints[1:]):
-        d = distance(prev, kp, cp)
-        if mind>d:
-            mind = d
-            tp,tpidx = (prev, kp), idx
-        prev = kp   
-        
-    return mind, tp, tpidx
+pidmodel = PidModel()
 
-def line_point_position(l1,l2, p):
-    return ((l2[0] - l1[0]) * (p[1] - l1[1]) - (l2[1]-l1[1]) * (p[0]-l1[0]))
+sequence = 0
 
-def get_nearest_point(keypoints, cp):
-    mind = 1000
-    min_idx = 0
-    for idx, kp in enumerate(keypoints):
-        d = line_dist(kp, cp)
-        if mind > d:
-            min_idx = idx
-            mind = d
-
-    return min_idx
-
-prev_point = [0,0]
-count = 0
-target_speed = 15.0
-prevcte = 0.0
 @sio.on('telemetry')
 def telemetry(sid, data):
-    global tpidx, count, target_speed, myrobot, prevcte, int_cte, prev_point
-    global taup, taud, taui
+    global sequence, model
     # The current steering angle of the car
     steering_angle = data["steering_angle"]
     # The current throttle of the car
     throttle = float(data["throttle"])
     # The current speed of the car
     speed = float(data["speed"])
-    print("Speed: ", speed)
     # The current image from the center camera of the car
     imgString = data["image"]
     position = data["position"]
-    px,py, _ = [float(x) for x in position.split(":")]
-    cp = (int(px), int(py))
 
     rotation = data["rotation"]
-    rx,ry,rz = [float(x) for x in rotation.split(":")]
 
-    tpidx = (get_nearest_point(keypoints, (px,py)) + 25) % len(keypoints)
+    _steering_angle, _throttle = pidmodel.predict(sequence, steering_angle, throttle, speed, imgString, position, rotation)
+    sequence += 1
+    send_control(_steering_angle, _throttle)
 
-    cv2.circle(img,(int(px),int(py)), 2, (255,255,255))
-    cv2.circle(img, keypoints[tpidx], 2, (255,255,0))
-    cv2.line(img, cp, keypoints[tpidx], (255, 0, 0))
-    cv2.imwrite("image.png", img)
-
-    car_orientation = deg2rad(ry-175.0-84.92393566084966)
-
-    target = keypoints[tpidx]
-
-    dir = line_point_position(prev_point, cp, target)
-
-    cte, _, _ = get_cte(keypoints, cp)
-    dcte = cte - prevcte * taud
-
-    steer = -cte * taup - dcte *taud - int_cte * taui
-
-    if dir < 0.0:
-            steer *= -1
-
-    int_cte += cte
-    print(prev_point, ",", cp, ",", target, "tpidx: ", tpidx, "car_orientation: ", rad2deg(car_orientation), "heading : ", rad2deg(steer))
-
-    steering_angle = steer
-    if count % 3 == 0:
-        prev_point = cp
-
-    if count % 50 == 0:
-        count = 0
-        int_cte = 0.0
-
-    if speed < target_speed:
-        throttle += 0.025
-    elif speed > target_speed:
-        throttle -= 0.025
-
-    send_control(steering_angle, throttle)
-    prevcte = cte
-    count += 1
 
 
 
@@ -163,7 +89,3 @@ if __name__ == '__main__':
 
     # deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
-
-
-# (1242, 682) (1213, 669) tpidx:  188 dist:  40.63088121429323 car_orientation:  -77.06213566084968 heading :  -155.85445803957836 31.78049716414141 - CORRECT ANGLE
-# (1243, 682) (1268, 655) tpidx:  188 dist:  40.30683719469939 car_orientation:  -78.75963566084967 heading :  -47.20259816176582 2.8284271247461903
---
+++ path_planning/model.py
@@ -0,0 +1,134 @@
+import numpy as np
+import time
+import cv2
+from robot import *
+from geo import *
+from drawing import *
+from dataprovider import *
+
+
+class PidModel:
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+
+        self.orientation = 0.0
+        self.taup = 0.001
+        self.taud = 0.01
+        self.taui = 0.0
+        self.int_cte = 0.0
+        self.sequence = -1
+
+        self.steering_angle = 0
+        self.target_speed = 10.0
+
+        self.prev_point = [0,0]
+        self.cte = 0.0
+
+
+        filename = 'data/000/driving_log.csv'
+        self.position_data = self.load_simulator_data(filename)
+
+        self.keypoints = []
+        self.pathimg = np.ones((1500, 1800, 3), np.uint8)
+
+        for idx, p in enumerate(self.position_data[280:]):
+            pos = (int(float(p[1][4])), int(float(p[1][5])))
+            cv2.circle(self.pathimg, pos, 1, (0, 0, 255))
+            self.keypoints.append(pos)
+
+    def load_simulator_data(self, csvfname):
+        """
+        Load dataset from csv file
+        """
+        data = []
+        with open(csvfname, 'r') as csvfile:
+            data_tmp = list(csv.reader(csvfile, delimiter=','))
+            for row in data_tmp:
+                x7 = [float(x) for x in row[7].split(':')]
+                x8 = [float(x) for x in row[8].split(':')]
+
+                data.append(((row[0], row[1], row[2]),
+                             np.array([float(row[3]), float(row[4]), float(row[5]), float(row[6])] + x7 + x8)))
+
+        return data
+
+    def set(self, x, y, orientation):
+        self.x = float(x)
+        self.y = float(y)
+        self.orientation = float(orientation)
+
+    def steer_turn_by(self, p):
+        l1, l2 = self.prev_point, (self.x, self.y)
+
+        return ((l2[0] - l1[0]) * (p[1] - l1[1]) - (l2[1] - l1[1]) * (p[0] - l1[0]))
+
+    def get_cte(self):
+        cp = (self.x, self.y)
+        mind = 10000.
+        min_idx = 0
+        for idx, kp in enumerate(self.keypoints):
+            d = line_dist(kp, cp)
+            if mind > d:
+                min_idx = idx
+                mind = d
+
+        return mind, min_idx
+
+    def adjust_throttle(self, speed):
+        if speed < self.target_speed:
+            self.throttle += 0.025
+        elif speed > self.target_speed:
+            self.throttle -= 0.025
+
+    def start_sequence(self, sequence):
+        if sequence > 50:
+            self.sequence = 0
+            self.int_cte = 0.0
+        else:
+            self.sequence = sequence
+
+    def _update(self):
+        cte, cte_idx = self.get_cte()
+        direction = self.steer_turn_by(self.keypoints[cte_idx])
+        dcte = cte - self.cte
+        steer = -cte * self.taup - dcte * self.taud - self.int_cte * self.taui
+
+        if direction < 0.0:
+            steer *= -1
+
+        """
+        cv2.circle(img, (int(px), int(py)), 2, (255, 255, 255))
+        cv2.circle(img, keypoints[tpidx], 2, (255, 255, 0))
+        cv2.line(img, cp, keypoints[tpidx], (255, 0, 0))
+        cv2.imwrite("image.png", img)
+        """
+        self.int_cte += cte
+        self.steering_angle = steer
+        self.cte = cte
+
+    def predict(self, sequence, steering_angle, throttle, speed, imgString, position, rotation):
+        self.steering_angle = steering_angle
+        self.throttle = throttle
+        self.speed = speed
+
+        if self.target_speed < speed:
+            self.throttle -= 0.01
+        elif self.target_speed > speed:
+            self.throttle += 0.01
+
+        self.x, self.y, self.z = [float(x) for x in position.split(":")]
+        rx, ry, rz = [float(x) for x in rotation.split(":")]
+        self.orientation = deg2rad(ry - 175.0 - 84.924)
+
+        self.start_sequence(sequence)
+        self._update()
+
+        self.update_prev_point()
+
+        return self.steering_angle, self.throttle
+
+    def update_prev_point(self):
+        if self.sequence % 3 == 0:
+            self.prev_point = (self.x, self.y)
