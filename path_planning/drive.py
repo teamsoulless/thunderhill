f9b00e7efc4afa06afce90d522644c645d5a4fe4
Maruf Maniruzzaman <kuasha@gmail.com>
2017-03-21 21:49:14 -0700

    Added D/I component

--- path_planning/drive.py
+++ path_planning/drive.py
@@ -11,7 +11,6 @@ from flask import Flask, render_template
 from io import BytesIO
 
 # Fix error with Keras and TensorFlow
-import cv2
 import matplotlib.pyplot as plt
 
 import cv2
@@ -27,10 +26,9 @@ position_data = load_simulator_data(filename)
 print(len(position_data))
 
 keypoints = []
-
 img = np.ones((1500,1800, 3), np.uint8) 
 
-for idx, p in enumerate (position_data[:1860]):
+for idx, p in enumerate (position_data[280:]):
     pos = (int(p[1][4]), int(p[1][5]))
     cv2.circle(img,pos, 1, (0,0,255))
         
@@ -41,27 +39,21 @@ for idx, p in enumerate (position_data[:1860]):
 tpidx = 2
 DEG25 = deg2rad(25) 
 
-taup = 0.25
+taup = 0.001
+taud = 0.01
+taui = 0.0
+int_cte = 0.0
 
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
 prev_image_array = None
 
-
-def can_move_next_tp(keypoints, tpidx, cp, change_at=10):
-    car_d=line_dist(cp, keypoints[tpidx])
-    print(cp, keypoints[tpidx], car_d)
-    if car_d<change_at:
-        return True
-        
-    return False
-
 def get_cte(keypoints, cp):
     prev=keypoints[0]
     tp = (keypoints[0], keypoints[1])
     tpidx = 1
-    mind = 1000.
+    mind = 50000.
     for idx, kp in enumerate(keypoints[1:]):
         d = distance(prev, kp, cp)
         if mind>d:
@@ -85,93 +77,72 @@ def get_nearest_point(keypoints, cp):
 
     return min_idx
 
-throttle = 0.1
-
-targets = [(1243.80, 704.22), (1245, 680) , (1268,655), (1213, 669)]
-curidx = 0
-
 prev_point = None
 count = 0
 target_speed = 15.0
+prevcte = 0.0
 @sio.on('telemetry')
 def telemetry(sid, data):
-    global tpidx, throttle, targets, curidx, prev_point, count, target_speed
+    global tpidx, count, target_speed, myrobot, prevcte, int_cte, prev_point
+    global taup, taud, taui
     # The current steering angle of the car
     steering_angle = data["steering_angle"]
     # The current throttle of the car
-    car_throttle = float(data["throttle"])
+    throttle = float(data["throttle"])
     # The current speed of the car
     speed = float(data["speed"])
+    print("Speed: ", speed)
     # The current image from the center camera of the car
     imgString = data["image"]
-    #print('speed:',data["speed"])
     position = data["position"]
     px,py, _ = [float(x) for x in position.split(":")]
     cp = (int(px), int(py))
-    #print('position: ', data["position"])
 
-    #_,_,tpidx=get_cte(keypoints, (px,py))
-    
     rotation = data["rotation"]
     rx,ry,rz = [float(x) for x in rotation.split(":")]
 
     tpidx = (get_nearest_point(keypoints, (px,py)) + 25) % len(keypoints)
-    #ptdist = line_dist(keypoints[tpidx], (px,py))
-
 
     cv2.circle(img,(int(px),int(py)), 2, (255,255,255))
     cv2.circle(img, keypoints[tpidx], 2, (255,255,0))
+    cv2.line(img, cp, keypoints[tpidx], (255, 0, 0))
     cv2.imwrite("image.png", img)
 
     car_orientation = deg2rad(ry-175.0-84.92393566084966)
-    #print("ry ", ry)
 
     target = keypoints[tpidx]
-    print((int(px),int(py)),", ", target)
 
-    steer = 0.0
-    if prev_point:
-        dir = line_point_position(prev_point, cp, target)
-        print("Direction: ", dir)
+    dir = line_point_position(prev_point, cp, target)
 
-        if dir < 0.0:
-            steer = 0.2
-        else:
-            steer = -0.2
+    cte, _, _ = get_cte(keypoints, cp)
+    dcte = cte - prevcte * taud
 
+    steer = -cte * taup - dcte *taud - int_cte * taui
 
+    if dir < 0.0:
+            steer *= -1
 
-    #steer = get_heading((int(px),int(py)), keypoints[tpidx])
-    #steer = get_heading(cp, target)
+    int_cte += cte
+
 
     print(prev_point, ",", cp, ",", target, "tpidx: ", tpidx, "car_orientation: ", rad2deg(car_orientation), "heading : ", rad2deg(steer))
 
-    #steer = (steer - car_orientation)
-    #steer = steer * taup / DEG25
-
-    """
-    steer = angle_trunc(steer)
-    if steer > DEG25:
-        steer = DEG25
-    if steer < -DEG25:
-        steer = -DEG25
-    """
-
     steering_angle = steer
-    #print(steering_angle)
-    # The driving model currently just outputs a constant throttle. Feel free to edit this.
-
-    print("control: ", steering_angle, throttle, tpidx)
-    #steering_angle = 0.32
-    send_control(steering_angle, throttle)
-    count += 1
     if count % 3 == 0:
         prev_point = cp
+
+    if count % 50 == 0:
+        count = 0
+        int_cte = 0.0
 
     if speed < target_speed:
         throttle += 0.025
     elif speed > target_speed:
         throttle -= 0.025
+
+    send_control(steering_angle, throttle)
+    prevcte = cte
+    count += 1
 
 
 
