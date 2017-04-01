573aff746a07639391e6ca51578b708040da36cc
Rana Khalil <rana.ae.khalil@gmail.com>
2017-03-31 20:39:19 -0700

    Testing images with color channels

--- testing/print_images.py
+++ testing/print_images.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import io
 from array import array
+from matplotlib import pyplot as plt
 
 fname='image.raw'
 file = open(fname, 'rb')
@@ -12,9 +13,19 @@ raw_image = Image.frombytes('RGB', [960,480], file.read(), 'raw')
 raw_image.save('raw.bmp')
 img = np.array(raw_image)
 file.close()
- 
+
+plt.imsave(fname.replace('.raw','.jpeg'), img)
 rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-cv2.imwrite(fname.replace('.raw','.jpeg'), rgb)
+cv2.imwrite('cv_' + fname.replace('.raw','.jpeg'), rgb)
+
+img_read = cv2.imread('image.jpeg')
+new_image = np.abs(img_read[:,:,0] - img[:,:,0])
+print(np.max(new_image))
+print(np.max(img_read))
+print(np.max(img))
+cv2.imwrite('image1.jpg',new_image)
+cv2.imwrite('image2.jpg',img_read)
+cv2.imwrite('image3.jpg',img)
 
 image_array = cv2.resize(img, (320, 160))
 cv2.imwrite('image_resized.jpg',image_array) 
@@ -26,6 +37,9 @@ hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
 cv2.imwrite('image_hls.jpg', hls)
 
 img_read = cv2.imread('image.jpeg', cv2.COLOR_BGR2RGB)
-img_raw = cv2.imread('raw.bmp')
+img_raw = cv2.imread('raw.bmp', cv2.COLOR_BGR2RGB)
 new_image = np.abs(img_read - img_raw)
+print(np.max(new_image))
+print(np.max(img_read))
+print(np.max(img_raw))
 cv2.imwrite('negative.jpg', new_image)
